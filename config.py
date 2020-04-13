c462dc2a0024ebe335810f01c651c6a4d018a502
LeoKnox <LeoKnox@users.noreply.github.com>
2020-04-13 13:28:59 -0700

    Add files via upload

--- config.py
+++ config.py
@@ -1,4 +1,6 @@
 import os
 
 class Config(object):
-    SECRET_KEY = os.envrion.get('SECRET_KEY') or "secrect_string"
+    SECRET_KEY = os.environ.get('SECRET_KEY') or "secrect_string"
+
+    MONGODB_SETTINGS = { 'db' : 'kanji_app_db' }
