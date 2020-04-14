78446005f95f672ed38186c392ecae2f950510a7
LeoKnox <LeoKnox@users.noreply.github.com>
2020-04-13 23:46:48 -0700

    Add files via upload

--- data_mod.py
+++ data_mod.py
@@ -3,10 +3,10 @@ with open('test_data.csv', 'r', encoding="utf8") as f:
     i = 0
     arr=[]
     for line in f:
-        print(type(line))
         x = line.split(",")
-        print(type(x))
-        print(x)
+        for y in x:
+            print(y)
         arr.append(x)
         i += 1
-    print (arr[0])
+    j = 0
+    #print (arr)
