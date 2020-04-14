26c8624a163483544434072c2cc6a3b518335859
LeoKnox <LeoKnox@users.noreply.github.com>
2020-04-13 22:38:20 -0700

    Add files via upload

--- data_mod.py
+++ data_mod.py
@@ -1,4 +1,10 @@
 with open('test_data.csv', 'r', encoding="utf8") as f:
     f.readline()
+    i = 0
     for line in f:
-        print (line)
+        if i%6:
+            print(line)
+            i += 1
+        else:
+            i += 1
+        #x = line.split(",")
