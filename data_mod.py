22ef835534e64807beb8849fe0bb8a30c79ff2b0
LeoKnox <LeoKnox@users.noreply.github.com>
2020-04-16 15:40:44 -0700

    Add files via upload

--- data_mod.py
+++ data_mod.py
@@ -11,7 +11,15 @@ with open('test_data.csv', 'r', encoding="utf8") as f:
     j = 0
     print(arr[0][0])
     #print (arr)
+    db_data = []
     for y in range(0, len(arr), 7):
         print ("-- {}".format(arr[y]))
-    db_data = {"translate":arr[0][0]}
+        item = {
+            'translation': arr[y][0],
+            'kanji': arr[y][1],
+            'pronunciation': arr[y][2],
+            #'kana': arr[y][3]
+        }
+        db_data.append(item)
+    #db_data = {"translate":arr[0][0]}
     print (db_data)
