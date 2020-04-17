99244dc60b4c31ac371a03e1016d293574d51f0c
LeoKnox <LeoKnox@users.noreply.github.com>
2020-04-16 22:51:09 -0700

    Add files via upload

--- data_mod.py
+++ data_mod.py
@@ -1,20 +1,24 @@
+import re
+
 #with open('test_data.csv', 'r', encoding="utf8") as f:
 with open('List-of-1000-Kanji-converted2.csv', 'r', encoding="utf8") as f:
     f.readline()
+    z = str(f)
     i = 0
     arr=[]
     for line in f:
         x = line.split(",")
         #for y in x:
             #print(y)
-        arr.append(x)
+        if (len(x) == 3):
+            arr.append(x)
         i += 1
     j = 0
     print(arr[0][0])
     #print (arr)
     db_data = []
-    for y in range(0, len(arr), 7):
-        print ("-- {}".format(arr[y]))
+    for y in range(0, len(arr)):
+        #print ("-- {}".format(arr[y]))
         item = {
             'translation': arr[y][0],
             'kanji': arr[y][1],
@@ -24,5 +28,5 @@ with open('List-of-1000-Kanji-converted2.csv', 'r', encoding="utf8") as f:
         db_data.append(item)
     #db_data = {"translate":arr[0][0]}
     #print (db_data)
-    f2 = open('kanji_db.csv', 'x')
-    f2.write(db_data)
+    f2 = open('kanji_db.csv', 'x', encoding="utf8")
+    f2.write(str(db_data))
