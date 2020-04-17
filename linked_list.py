e4cb3e7f565fcbcbeaa0f49c5abd1a47493693f5
LeoKnox <LeoKnox@users.noreply.github.com>
2020-04-17 15:01:10 -0700

    Add files via upload

--- linked_list.py
+++ linked_list.py
@@ -7,17 +7,32 @@ class head:
         while (temp):
             print (temp.val),
             temp = temp.next
+    
+    def addNode(self, val):
+        newNode = node(val)
+        newNode.next = self.head
+        self.head = newNode
+    
+    def addEnd(self, newVal):
+        newNode = node(newVal)
+        temp = self.head
+        while (temp.next):
+            temp = temp.next
+        temp.next = newNode
 
 class node:
     def __init__(self, val):
         self.val = val
         self.next = None
 
-print("a")
+print("start here")
 l_one = head()
-temp = head()
-l_one.head = node("t")
-new_node = node("u")
-l_one.head.next = new_node
-print(l_one.head.next.val)
+l_one.addNode("t")
+l_one.addNode("s")
+l_one.addEnd("y")
 l_one.printList()
+#temp = head()
+#l_one.head = node("t")
+#new_node = node("u")
+#l_one.head.next = new_node
+#print(l_one.head.next.val)
