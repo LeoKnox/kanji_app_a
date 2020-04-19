094f70d87ee75f966619242608bafe8330e57316
LeoKnox <LeoKnox@users.noreply.github.com>
2020-04-19 16:49:01 -0700

    Add files via upload

--- tree.py
+++ tree.py
@@ -1,57 +1,11 @@
-class tree:
-    def __init__(self):
-        self.head = None
-    
-    def recNode(self, term):
-        if term < self.head.info:
-            if self.head.left is None:
-                self.head.left = Node(data)
-            else:
-                self.head.left.recNode(data)
-        elif term < self.head.info:
-            if self.head.right is None:
-                self.head.right= Node(data)
-            else:
-                self.head.right.recNode(data)
-    
-    
-    def findNode(self, term):
-        if self.head == None:
-            return False
-        temp = self.head
-        if self.head.info == term:
-            return True
-
-    def add_node(self, info):
-        newNode = node(info)
-        if self.head == None:
-            self.head = newNode
-            return
-        temp = self.head
-        while temp != None:
-            if info < temp.info:
-                if temp.left == None:
-                    temp.left = newNode
-                else:
-                    temp = temp.left
-                    return
-            else:
-                temp.right = newNode
-                return
-
-class node:
+class Tree:
     def __init__(self, info):
-        self.info = info
-        self.right = None
         self.left = None
+        self.right = None
+        self.info = info
+    
+    def traceTree(self):
+        print(self.info)
 
-t = tree()
-t.add_node(7)
-t.add_node(3)
-t.add_node(9)
-t.add_node(1)
-t.recNode(8)
-n = node(5)
-print (t.head.right.info)
-
-print (n.info)
+t = Tree(7)
+t.traceTree()
