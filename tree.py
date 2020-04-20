e5009f821fa06a8f0ecb9aebe0925b78af104af6
LeoKnox <LeoKnox@users.noreply.github.com>
2020-04-19 23:40:58 -0700

    Add files via upload

--- tree.py
+++ tree.py
@@ -7,20 +7,24 @@ class Tree:
     def delNode(self, info):
         if self.info == info:
             print('equal')
-            if not self.left and self.right:
+            if not self.left and not self.right:
                 print('a')
                 self.left = None
                 self.right = None
                 self.info = None
             elif self.right:
                 print('aa')
-                self.right.delNode(self.info)
+                self.left = None
+                self.right = None
+                self.info = None
             else:
                 print('d')
         elif info <= self.info:
+            print('e')
             self.left.delNode(self.info)
         elif info > self.info:
-            self.right.delNode(self.info)
+            print('f')
+            self.delNode(self.info)
     
     def addNode(self, info):
         if self.info:
@@ -46,6 +50,6 @@ t = Tree(7)
 t.addNode(8)
 t.addNode(3)
 t.traceTree()
-t.delNode(7)
+t.delNode(3)
 print('99999')
 t.traceTree()
